824efc8402d239f99359f1e2043160e022a47efc
Martin A. Jackson <martin.a.jackson@gmail.com>
2019-07-26 13:19:47 -0500

    Docz working demo

---
+++ hooks/NameForm.js
@@ -0,0 +1,31 @@
+
+// Taken from https://rangle.io/blog/simplifying-controlled-inputs-with-hooks/
+
+import React from "react";
+import { useInput } from './hooks/input-hook';
+
+
+export function NameForm(props) {
+  const { value:firstName, bind:bindFirstName, reset:resetFirstName } = useInput('');
+  const { value:lastName, bind:bindLastName, reset:resetLastName } = useInput('');
+
+  const handleSubmit = (evt) => {
+      evt.preventDefault();
+      alert(`Submitting Name ${firstName} ${lastName}`);
+      resetFirstName();
+      resetLastName();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        First Name:
+        <input type="text" {...bindFirstName} />
+      </label>
+      <label>
+        Last Name:
+        <input type="text" {...bindLastName} />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
